927d386d964191af64f7cb62122d61bdb60ea9fc
Prish20 <prish@students.uonbi.ac.ke>
2024-09-11 03:18:13 +0300

    Fix file requirement fail

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+"""
+This module creates a basic Flask app
+that serves an HTML page with a welcome title and header.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    """Renders the index page"""
+def index() -> str:
+    """
+    Renders the index page.
+
+    Returns:
+        str: The rendered HTML template for the index page.
+    """
     return render_template('0-index.html')
 
 
